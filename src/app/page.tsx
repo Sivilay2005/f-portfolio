848689611a37d1259476a1f516381ff5fa6c321b
Sivilay2005 <sivilaisibounheuang@gmail.com>
2025-07-15 12:14:43 +0700

    � Fix: remove unused imports in page.tsx for Render build

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,4 @@
-import Image from "next/image";
-
-//components
-import NavBar from "./components/NavBar";
 import Banner from "./components/Banner";
-import { Contact } from "lucide-react";
 export default function Home() {
   return (
     <div>
